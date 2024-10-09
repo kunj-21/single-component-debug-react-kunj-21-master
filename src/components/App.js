632f15d833805da43b8c6ca2390039a704c58ba6
KunjP-2528 <kunj@vrize.com>
2024-10-09 22:57:24 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+    </div>
+  )
+}
+
+export default App
+
+
+
+
